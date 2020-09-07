f438959d61d96827c2772f30d2a91fc7d6c4d0b8
Matt Nathan <matt.nathan@vanti.co.uk>
2020-09-07 11:37:27 +0100

    feat: add trait name constants

---
+++ pkg/trait/names.go
@@ -0,0 +1,21 @@
+package trait
+
+const (
+	Audit             = "smartcore.device.traits.v1.Audit"
+	Booking           = "smartcore.device.traits.v1.Booking"
+	Brightness        = "smartcore.device.traits.v1.Brightness"
+	ExtendRetract     = "smartcore.device.traits.v1.ExtendRetract"
+	Health            = "smartcore.device.traits.v1.Health"
+	HumiditySensor    = "smartcore.device.traits.v1.HumiditySensor"
+	InputSelect       = "smartcore.device.traits.v1.InputSelect"
+	Microphone        = "smartcore.device.traits.v1.Microphone"
+	MotionSensor      = "smartcore.device.traits.v1.MotionSensor"
+	Occupancy         = "smartcore.device.traits.v1.Occupancy"
+	OnOff             = "smartcore.device.traits.v1.OnOff"
+	OpenClose         = "smartcore.device.traits.v1.OpenClose"
+	Ptz               = "smartcore.device.traits.v1.Ptz"
+	Range             = "smartcore.device.traits.v1.Range"
+	Speaker           = "smartcore.device.traits.v1.Speaker"
+	TemperatureSensor = "smartcore.device.traits.v1.TemperatureSensor"
+	Thermostat        = "smartcore.device.traits.v1.Thermostat"
+)

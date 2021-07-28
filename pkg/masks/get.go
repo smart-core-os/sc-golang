b74c4506997aed91292aee0cf6eb2d1db8c5cbb8
Matt Nathan <matt.nathan@vanti.co.uk>
2021-07-28 10:43:29 +0100

    fix: add ResponseFilter for responding to requests and applying field masks

---
+++ pkg/masks/get.go
@@ -0,0 +1,75 @@
+package masks
+
+import (
+	"github.com/mennanov/fmutils"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+)
+
+// ResponseFilter provides utilities for applying FieldMasks to responses.
+type ResponseFilter struct {
+	fields *fieldmaskpb.FieldMask
+}
+
+// NewResponseFilter creates a new ResponseFilter with the given options applied.
+func NewResponseFilter(opts ...ResponseFilterOption) *ResponseFilter {
+	res := &ResponseFilter{}
+	for _, opt := range opts {
+		opt(res)
+	}
+	return res
+}
+
+func (r *ResponseFilter) Validate(msg proto.Message) error {
+	if r.fields != nil {
+		if !r.fields.IsValid(msg) {
+			return status.Errorf(codes.InvalidArgument, "%v mentions unknown fields", r.fields)
+		}
+	}
+	return nil
+}
+
+func (r *ResponseFilter) Filter(msg proto.Message) {
+	if r.fields == nil {
+		return
+	}
+	if len(r.fields.GetPaths()) == 0 {
+		proto.Reset(msg)
+		return
+	}
+	fmutils.Filter(msg, r.fields.GetPaths())
+}
+
+func (r *ResponseFilter) FilterClone(msg proto.Message) proto.Message {
+	if r.fields == nil {
+		return msg
+	}
+	if len(r.fields.GetPaths()) == 0 {
+		clone := proto.Clone(msg)
+		proto.Reset(clone)
+		return clone
+	}
+	clone := proto.Clone(msg)
+	fmutils.Filter(clone, r.fields.GetPaths())
+	return clone
+}
+
+type ResponseFilterOption func(*ResponseFilter)
+
+var emptyResponseFilterOption ResponseFilterOption = func(_ *ResponseFilter) {
+}
+
+func WithFieldMask(fm *fieldmaskpb.FieldMask) ResponseFilterOption {
+	if fm == nil {
+		return emptyResponseFilterOption
+	}
+	return func(filter *ResponseFilter) {
+		filter.fields = fm
+	}
+}
+
+func WithFieldMaskPaths(paths ...string) ResponseFilterOption {
+	return WithFieldMask(&fieldmaskpb.FieldMask{Paths: paths})
+}

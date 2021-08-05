a32cc8ba0df2d0f8a190e76b4a983ce1ef311bfb
Matt Nathan <matt.nathan@vanti.co.uk>
2021-08-05 12:04:26 +0100

    feat: add a collection of GrpcApis as a thing

--- pkg/server/api.go
+++ pkg/server/api.go
@@ -5,3 +5,16 @@ import "google.golang.org/grpc"
 type GrpcApi interface {
 	Register(server *grpc.Server)
 }
+
+type collection []GrpcApi
+
+func (c collection) Register(server *grpc.Server) {
+	for _, api := range c {
+		api.Register(server)
+	}
+}
+
+// Collection combines multiple GrpcApi instances into a single GrpcApi that all get registered at the same time.
+func Collection(apis ...GrpcApi) GrpcApi {
+	return collection(apis)
+}

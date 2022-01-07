a011b4b982dfb4b60b615e0c433dd7b7e837f0a7
Matt Nathan <matt.nathan@vanti.co.uk>
2022-01-07 09:38:59 +0000

    dev: add generated wrapper code for electric trait

--- pkg/trait/electric/gen.go
+++ pkg/trait/electric/gen.go
@@ -2,3 +2,4 @@ package electric
 
 // PREREQUISITE: protoc-gen-router is on PATH, i.e. `go install github.com/smart-core-os/sc-golang/cmd/protoc-gen-router`
 //go:generate protoc -I ../../../.protomod -I ../../../.protomod/github.com/smart-core-os/sc-api/protobuf/ -I ../../.. --router_out=../../.. pkg/trait/electric/memory_settings.proto github.com/smart-core-os/sc-api/protobuf/traits/electric.proto
+//go:generate protoc -I ../../../.protomod -I ../../../.protomod/github.com/smart-core-os/sc-api/protobuf/ -I ../../.. --wrapper_out=../../.. pkg/trait/electric/memory_settings.proto github.com/smart-core-os/sc-api/protobuf/traits/electric.proto

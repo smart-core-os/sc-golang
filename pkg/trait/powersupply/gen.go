7eb588f29d81031ad7d75e1bcc2278609f870a34
Matt Nathan <matt.nathan@vanti.co.uk>
2022-01-10 11:33:58 +0000

    chore: replace power supply router and wrapper with generated versions

---
+++ pkg/trait/powersupply/gen.go
@@ -0,0 +1,6 @@
+package powersupply
+
+// PREREQUISITE: protocmod is on PATH, i.e. `go install git.vanti.co.uk/vanti-incubator/protomod`
+// PREREQUISITE: protoc-gen-router is on PATH, i.e. `go install github.com/smart-core-os/sc-golang/cmd/protoc-gen-router`
+// PREREQUISITE: protoc-gen-wrapper is on PATH, i.e. `go install github.com/smart-core-os/sc-golang/cmd/protoc-gen-wrapper`
+//go:generate protomod protoc -- -I ../../.. --router_out=../../.. --wrapper_out=../../.. pkg/trait/powersupply/memory_settings.proto github.com/smart-core-os/sc-api/protobuf/traits/power_supply.proto

23e42f1bf669e36acb6006822b027ab58ea9abe5
Matt Nathan <matt.nathan@vanti.co.uk>
2022-01-10 11:29:20 +0000

    feat: add proto.mod and replace generators with protomod versions

--- pkg/trait/electric/gen.go
+++ pkg/trait/electric/gen.go
@@ -1,5 +1,7 @@
 package electric
 
+// PREREQUISITE: protocmod is on PATH, i.e. `go install git.vanti.co.uk/vanti-incubator/protomod`
 // PREREQUISITE: protoc-gen-router is on PATH, i.e. `go install github.com/smart-core-os/sc-golang/cmd/protoc-gen-router`
-//go:generate protoc -I ../../../.protomod -I ../../../.protomod/github.com/smart-core-os/sc-api/protobuf/ -I ../../.. --router_out=../../.. pkg/trait/electric/memory_settings.proto github.com/smart-core-os/sc-api/protobuf/traits/electric.proto
-//go:generate protoc -I ../../../.protomod -I ../../../.protomod/github.com/smart-core-os/sc-api/protobuf/ -I ../../.. --wrapper_out=../../.. pkg/trait/electric/memory_settings.proto github.com/smart-core-os/sc-api/protobuf/traits/electric.proto
+// PREREQUISITE: protoc-gen-wrapper is on PATH, i.e. `go install github.com/smart-core-os/sc-golang/cmd/protoc-gen-wrapper`
+//go:generate protomod protoc -- -I ../../.. --router_out=../../.. pkg/trait/electric/memory_settings.proto github.com/smart-core-os/sc-api/protobuf/traits/electric.proto
+//go:generate protomod protoc -- -I ../../.. --wrapper_out=../../.. pkg/trait/electric/memory_settings.proto github.com/smart-core-os/sc-api/protobuf/traits/electric.proto
---
+++ proto.mod
@@ -0,0 +1,5 @@
+ module github.com/smart-core-os/sc-golang
+
+require (
+  github.com/smart-core-os/sc-api/protobuf v1.0.0-beta.8.0.20211224080015-835f9b034a1c
+)

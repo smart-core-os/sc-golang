90f1cf5829d031073c8fee80560cb22302639a6d
Matt Nathan <matt.nathan@vanti.co.uk>
2022-01-11 10:23:40 +0000

    chore: protomod is now on github.com

--- pkg/trait/occupancysensor/gen.go
+++ pkg/trait/occupancysensor/gen.go
@@ -1,6 +1,6 @@
 package occupancysensor
 
-// PREREQUISITE: protocmod is on PATH, i.e. `go install git.vanti.co.uk/vanti-incubator/protomod`
+// PREREQUISITE: protocmod is on PATH, i.e. `go install github.com/smart-core-os/protomod`
 // PREREQUISITE: protoc-gen-router is on PATH, i.e. `go install github.com/smart-core-os/sc-golang/cmd/protoc-gen-router`
 // PREREQUISITE: protoc-gen-wrapper is on PATH, i.e. `go install github.com/smart-core-os/sc-golang/cmd/protoc-gen-wrapper`
 //go:generate protomod protoc -- -I ../../.. --router_out=../../.. --wrapper_out=../../.. github.com/smart-core-os/sc-api/protobuf/traits/occupancy_sensor.proto

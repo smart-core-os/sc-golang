d0ce737c8674f56e2a07d9f2c047a73bb49433e4
Matt Nathan <matt.nathan@vanti.co.uk>
2022-03-24 15:56:20 +0000

    feat(cmp): allow equality based on relative proximity of durations (i.e. within a percentage)

--- pkg/cmp/time.go
+++ pkg/cmp/time.go
@@ -38,30 +38,54 @@ func toTime(x pref.Message) time.Time {
 	return x.Interface().(*timestamppb.Timestamp).AsTime()
 }
 
+// DurationValueWithin considers two durationpb.Duration to be equal if their durations are within d of each other.
 func DurationValueWithin(d time.Duration) Value {
-	return func(fd pref.FieldDescriptor, x, y pref.Value) (equal bool, ok bool) {
-		if fd.Kind() != pref.MessageKind {
-			return false, false
+	return func(fd pref.FieldDescriptor, x, y pref.Value) (equal, ok bool) {
+		xd, yd, equal, ok, returnEarly := cmpDuration(fd, x, y)
+		if returnEarly {
+			return equal, ok
 		}
-		mx, my := x.Message(), y.Message()
-		xIsDuration := mx.Descriptor().FullName() == "google.protobuf.Duration"
-		yIsDuration := my.Descriptor().FullName() == "google.protobuf.Duration"
-		if !xIsDuration && !yIsDuration {
-			return false, false
-		}
-		if xIsDuration != yIsDuration {
-			return false, true
-		}
-		if !mx.IsValid() || !my.IsValid() {
-			return mx.IsValid() == my.IsValid(), true
-		}
-
-		xd, yd := toDuration(mx), toDuration(my)
 		if xd < yd {
 			return yd-xd <= d, true
 		}
 		return xd-yd <= d, true
 	}
+}
+
+// DurationValueWithinP considers two durationpb.Duration to be equal if their values are within p percent of each other.
+func DurationValueWithinP(p float32) Value {
+	return func(fd pref.FieldDescriptor, x, y pref.Value) (equal, ok bool) {
+		xd, yd, equal, ok, returnEarly := cmpDuration(fd, x, y)
+		if returnEarly {
+			return equal, ok
+		}
+		pd := float32(xd) / float32(yd)
+		if pd < 0 {
+			pd = -pd
+		}
+		return pd < p, true
+	}
+}
+
+func cmpDuration(fd pref.FieldDescriptor, x, y pref.Value) (xd, yd time.Duration, equal, ok, returnEarly bool) {
+	if fd.Kind() != pref.MessageKind {
+		return xd, yd, false, false, true
+	}
+	mx, my := x.Message(), y.Message()
+	xIsDuration := mx.Descriptor().FullName() == "google.protobuf.Duration"
+	yIsDuration := my.Descriptor().FullName() == "google.protobuf.Duration"
+	if !xIsDuration && !yIsDuration {
+		return xd, yd, false, false, true
+	}
+	if xIsDuration != yIsDuration {
+		return xd, yd, false, true, true
+	}
+	if !mx.IsValid() || !my.IsValid() {
+		return xd, yd, mx.IsValid() == my.IsValid(), true, true
+	}
+
+	xd, yd = toDuration(mx), toDuration(my)
+	return xd, yd, false, false, false
 }
 
 func toDuration(x pref.Message) time.Duration {

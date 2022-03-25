1a104044b20866c6919a18fda5a5d95e03c8a92c
Matt Nathan <matt.nathan@vanti.co.uk>
2022-03-25 12:05:51 +0000

    doc: fix comment saying protocmod is needed, it's protomod

--- pkg/trait/light/gen.go
+++ pkg/trait/light/gen.go
@@ -1,6 +1,6 @@
 package light
 
-// PREREQUISITE: protocmod is on PATH, i.e. `go install github.com/smart-core-os/protomod`
+// PREREQUISITE: protomod is on PATH, i.e. `go install github.com/smart-core-os/protomod`
 // PREREQUISITE: protoc-gen-router is on PATH, i.e. `go install github.com/smart-core-os/sc-golang/cmd/protoc-gen-router`
 // PREREQUISITE: protoc-gen-wrapper is on PATH, i.e. `go install github.com/smart-core-os/sc-golang/cmd/protoc-gen-wrapper`
 //go:generate protomod protoc -- -I ../../.. --router_out=../../.. --wrapper_out=../../.. github.com/smart-core-os/sc-api/protobuf/traits/light.proto

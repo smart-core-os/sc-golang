99f597124f5c80ad121986d02b4389155b5f8b9e
Matt Randell <Matt.Randell@vanti.co.uk>
2022-03-30 13:20:40 +0100

    fix(openclose): tidy up based on review feedback
    https://github.com/smart-core-os/sc-golang/pull/1#pullrequestreview-925932418

--- pkg/trait/openclose/model.go
+++ pkg/trait/openclose/model.go
@@ -7,29 +7,19 @@ import (
 	"github.com/smart-core-os/sc-api/go/traits"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/proto"
 
 	"github.com/smart-core-os/sc-golang/pkg/cmp"
 	"github.com/smart-core-os/sc-golang/pkg/resource"
 )
 
-type Preset struct {
-	Name       string
-	Percentage float32
-}
-
-// Model provides a data structure for supporting the Fan Speed trait.
-// Unless configured otherwise, this model supports DefaultPresets presets.
+// Model provides a data structure for supporting the Open Close trait.
 type Model struct {
 	positions *resource.Value // of *traits.OpenClosePositions
 }
 
-// NewModel constructs a Model with default values using the first of DefaultPresets.
+// NewModel constructs a Model with default values
 func NewModel() *Model {
-	position := &traits.OpenClosePosition{
-		OpenPercent: 0,
-		Direction:   traits.OpenClosePosition_UNSPECIFIED,
-	}
+	position := &traits.OpenClosePosition{}
 	positions := &traits.OpenClosePositions{
 		States: []*traits.OpenClosePosition{position},
 	}
@@ -51,18 +41,11 @@ func (m *Model) Positions(opts ...resource.ReadOption) *traits.OpenClosePosition
 
 // UpdatePositions changes the positions
 // Provide write options to support features like relative changes.
-// Related properties of the positions will be set automatically,
-// for example if the open_percent is set, then target_open_percent will be modified accordingly.
-// Providing a resource.InterceptAfter option will disable related property updating.
-// Using DeriveValues can re-enable related property computation if needed.
-func (m *Model) UpdatePositions(positions *traits.OpenClosePositions, opts ...resource.WriteOption) (
-	*traits.OpenClosePositions, error,
-) {
+func (m *Model) UpdatePositions(positions *traits.OpenClosePositions, opts ...resource.WriteOption) (*traits.OpenClosePositions, error) {
 	if err := m.validateUpdate(positions); err != nil {
 		return nil, err
 	}
 
-	opts = append([]resource.WriteOption{resource.InterceptAfter(m.DeriveValues)}, opts...)
 	val, err := m.positions.Set(positions, opts...)
 	if val == nil {
 		return nil, err
@@ -76,34 +59,17 @@ func (m *Model) validateUpdate(positions *traits.OpenClosePositions) error {
 		if percent < 0 || percent > 100 {
 			return status.Errorf(codes.InvalidArgument, "open_percent %f is out of range 0-100", percent)
 		}
-
-		if position.GetOpenPercentTween() != nil {
-			return status.Error(codes.Unimplemented, "tweening support not implemented")
-		}
 	}
 	return nil
 }
 
-// DeriveValues derives values that haven't been set from those that have.
-// For example setting percentage when the preset changes.
-// Updated values will be set on new.
-func (m *Model) DeriveValues(old, new proto.Message) {
-	newVal := new.(*traits.OpenClosePositions)
-
-	for _, position := range newVal.GetStates() {
-		position.TargetOpenPercent = position.OpenPercent
-	}
-
-}
-
-//goland:noinspection GoNameStartsWithPackageName
-type OpenCloseChange struct {
+type PositionsChange struct {
 	Value      *traits.OpenClosePositions
 	ChangeTime time.Time
 }
 
-func (m *Model) PullPositions(ctx context.Context, opts ...resource.ReadOption) <-chan OpenCloseChange {
-	send := make(chan OpenCloseChange)
+func (m *Model) PullPositions(ctx context.Context, opts ...resource.ReadOption) <-chan PositionsChange {
+	send := make(chan PositionsChange)
 	go func() {
 		defer close(send)
 		for change := range m.positions.Pull(ctx, opts...) {
@@ -111,7 +77,7 @@ func (m *Model) PullPositions(ctx context.Context, opts ...resource.ReadOption) 
 			select {
 			case <-ctx.Done():
 				return
-			case send <- OpenCloseChange{Value: val, ChangeTime: change.ChangeTime}:
+			case send <- PositionsChange{Value: val, ChangeTime: change.ChangeTime}:
 			}
 		}
 	}()

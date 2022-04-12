4582545ba8f850ec0ef76f3d60da0cc4490540eb
Matt Randell <Matt.Randell@vanti.co.uk>
2022-04-12 10:12:23 +0100

    dev: add minibus package to hopefully replace the emitter

---
+++ internal/minibus/bus.go
@@ -0,0 +1,115 @@
+package minibus
+
+import (
+	"context"
+	"sync"
+)
+
+type Bus struct {
+	listenerM sync.RWMutex
+	listeners []*listener
+}
+
+func (b *Bus) Send(ctx context.Context, event interface{}) (ok bool) {
+	// create a copy of the listeners so avoid holding the mutex a long time
+	var listeners []*listener
+	b.listenerM.RLock()
+	for _, l := range b.listeners {
+		listeners = append(listeners, l)
+	}
+	b.listenerM.RUnlock()
+
+	needGc := false
+
+	// send the event to each listener that's not closed
+	for _, l := range listeners {
+		ok, active := l.send(ctx, event)
+		if !ok {
+			return false
+		}
+		if !active {
+			// the listen context on this listener has been cancelled, we need to collect the garbage
+			needGc = true
+		}
+	}
+
+	if needGc {
+		b.collect()
+	}
+
+	return true
+}
+
+func (b *Bus) collect() {
+	b.listenerM.Lock()
+	defer b.listenerM.Unlock()
+
+	var activeListeners []*listener
+	for _, l := range b.listeners {
+		if l.alive() {
+			activeListeners = append(activeListeners, l)
+		}
+	}
+
+	b.listeners = activeListeners
+}
+
+func (b *Bus) Listen(ctx context.Context) <-chan interface{} {
+	ch := make(chan interface{})
+
+	l := &listener{
+		ch:  ch,
+		ctx: ctx,
+	}
+
+	go func() {
+		<-ctx.Done()
+		l.stop()
+	}()
+
+	// store the listener
+	b.listenerM.Lock()
+	defer b.listenerM.Unlock()
+	b.listeners = append(b.listeners, l)
+
+	return ch
+}
+
+type listener struct {
+	m   sync.RWMutex
+	ch  chan interface{}
+	ctx context.Context
+}
+
+func (l *listener) send(ctx context.Context, event interface{}) (ok bool, active bool) {
+	l.m.RLock()
+	defer l.m.RUnlock()
+
+	select {
+	case <-ctx.Done():
+		// send context cancelled
+		return false, true
+
+	case <-l.ctx.Done():
+		// listen context cancelled
+		// this is considered a success even though the message is not sent
+		return true, false
+
+	case l.ch <- event:
+		// event sent successfully
+		return true, true
+	}
+}
+
+func (l *listener) stop() {
+	l.m.Lock()
+	defer l.m.Unlock()
+	if l.ch != nil {
+		close(l.ch)
+		l.ch = nil
+	}
+}
+
+func (l *listener) alive() bool {
+	return l.ctx.Err() != nil
+}
---
+++ internal/minibus/bus_test.go
@@ -0,0 +1,55 @@
+package minibus
+
+import (
+	"context"
+	"sync"
+	"testing"
+)
+
+func TestBus_OneToMany(t *testing.T) {
+	var bus Bus
+
+	listenCtx, stopListen := context.WithCancel(context.Background())
+
+	// start the listeners
+	const numListeners = 10
+	var listenChs []<-chan interface{}
+	for i := 0; i < numListeners; i++ {
+		listenChs = append(listenChs, bus.Listen(listenCtx))
+	}
+
+	// one goroutine sends
+	go func() {
+		defer stopListen()
+		for i := 0; i < 100; i++ {
+			bus.Send(context.Background(), i)
+		}
+	}()
+
+	// several goroutines should all receive all the elements
+	var group sync.WaitGroup
+	for listenIndex, listenCh := range listenChs {
+		listenIndex, listenCh := listenIndex, listenCh
+		group.Add(1)
+		go func() {
+			defer group.Done()
+			collected := collector(listenCh)
+			if len(collected) != 100 {
+				t.Errorf("{%d} expected to collect 100 items but got %d", listenIndex, len(collected))
+				return
+			}
+			for i := 0; i < 100; i++ {
+				if collected[i].(int) != i {
+					t.Errorf("{%d} collected[%d] = %d", listenIndex, i, collected[i])
+				}
+			}
+		}()
+	}
+}
+
+func collector(source <-chan interface{}) (collected []interface{}) {
+	for data := range source {
+		collected = append(collected, data)
+	}
+	return
+}

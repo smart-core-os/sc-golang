1efd8a35601814596da6694f7b14739c0eb99ed5
Matt Nathan <matt.nathan@vanti.co.uk>
2024-01-04 15:52:09 +0000

    refactor: replace airqualitysensor.NewModel initialValue with ...resource.Option

---
+++ pkg/trait/airqualitysensor/model_opts.go
@@ -0,0 +1,62 @@
+package airqualitysensor
+
+import (
+	"github.com/smart-core-os/sc-api/go/traits"
+	"github.com/smart-core-os/sc-golang/pkg/resource"
+)
+
+// DefaultModelOptions holds the default options for the model.
+var DefaultModelOptions = []resource.Option{
+	WithInitialAirQuality(&traits.AirQuality{}),
+}
+
+// ModelOption defined the base type for all options that apply to this traits model.
+type ModelOption interface {
+	resource.Option
+	applyModel(args *modelArgs)
+}
+
+// WithAirQualityOption configures the airQuality resource of the model.
+func WithAirQualityOption(opts ...resource.Option) resource.Option {
+	return modelOptionFunc(func(args *modelArgs) {
+		args.airQualityOpts = append(args.airQualityOpts, opts...)
+	})
+}
+
+// WithInitialAirQuality returns an option that configures the model to initialise with the given air quality.
+func WithInitialAirQuality(airQuality *traits.AirQuality) resource.Option {
+	return WithAirQualityOption(resource.WithInitialValue(airQuality))
+}
+
+func calcModelArgs(opts ...resource.Option) modelArgs {
+	args := new(modelArgs)
+	args.apply(DefaultModelOptions...)
+	args.apply(opts...)
+	return *args
+}
+
+type modelArgs struct {
+	airQualityOpts []resource.Option
+}
+
+func (a *modelArgs) apply(opts ...resource.Option) {
+	for _, opt := range opts {
+		if v, ok := opt.(ModelOption); ok {
+			v.applyModel(a)
+		}
+		a.airQualityOpts = append(a.airQualityOpts, opt)
+	}
+}
+
+func modelOptionFunc(fn func(args *modelArgs)) ModelOption {
+	return modelOption{resource.EmptyOption{}, fn}
+}
+
+type modelOption struct {
+	resource.Option
+	fn func(args *modelArgs)
+}
+
+func (m modelOption) applyModel(args *modelArgs) {
+	m.fn(args)
+}

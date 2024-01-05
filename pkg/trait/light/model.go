84fe5790f04d837da14250cea3873dfbdcd22852
Tom Haskell <tom.haskell@vanti.co.uk>
2024-01-05 08:37:30 +0000

    feat(trait.Light): add Model and ModelServer (uses Brightness) (#2)

---
+++ pkg/trait/light/model.go
@@ -0,0 +1,54 @@
+package light
+
+import (
+	"context"
+	"time"
+
+	"github.com/smart-core-os/sc-api/go/traits"
+	"github.com/smart-core-os/sc-golang/pkg/resource"
+)
+
+type Model struct {
+	brightness *resource.Value
+}
+
+func NewModel(initialState *traits.Brightness) *Model {
+	return &Model{
+		brightness: resource.NewValue(resource.WithInitialValue(initialState)),
+	}
+}
+
+func (m *Model) GetBrightness(opts ...resource.ReadOption) (*traits.Brightness, error) {
+	return m.brightness.Get(opts...).(*traits.Brightness), nil
+}
+
+func (m *Model) UpdateBrightness(light *traits.Brightness, opts ...resource.WriteOption) (*traits.Brightness, error) {
+	res, err := m.brightness.Set(light, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return res.(*traits.Brightness), nil
+}
+
+func (m *Model) PullBrightness(ctx context.Context, opts ...resource.ReadOption) <-chan PullBrightnessChange {
+	send := make(chan PullBrightnessChange)
+
+	recv := m.brightness.Pull(ctx, opts...)
+	go func() {
+		defer close(send)
+		for change := range recv {
+			value := change.Value.(*traits.Brightness)
+			send <- PullBrightnessChange{
+				Value:      value,
+				ChangeTime: change.ChangeTime,
+			}
+		}
+	}()
+
+	return send
+}
+
+type PullBrightnessChange struct {
+	Value      *traits.Brightness
+	ChangeTime time.Time
+}

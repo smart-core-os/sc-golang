6da5e7b406537e6fc89d06304f94488c834e1aac
Matt Randell <matt.randell@vanti.co.uk>
2024-09-10 13:51:47 +0100

    wrap: propagate metadata through ServerToClient calls
    Propagate gRPC header metadata through calls that
    use a ClientConnInterface returned from ServerToClient.
    
    Convert outgoing metadata at the call site to incoming
    metadata for the handler.
    
    Attach a TransportStream to the request context, so that
    grpc.SetHeader can work (this is the only way to set
    response headers for unary RPCs).
    
    Support grpc.Header and grpc.Trailer CallOptions for
    unary RPCs (in wrapper.Invoke).
    
    Fix ClientServerStream bug where retrieving metadata
    from a ClientStream was unreliable after the stream
    had closed.
    
    Fix a ClientServerStream bug where calling
    ClientStream.CloseSend() caused a panic on future
    ServerStream.Send(), which is not part of the stream
    API contract:
    
    Comment from @lyuxuan https://github.com/grpc/grpc-go/issues/2015#issuecomment-382958126:
    
    > CloseSend() is called by client to signal server that it has sent all the messages it want to send, and will send no more message
    > [...]
    > However, server is still free to send whatever message(s) it wants to send to client, and rpc is still alive. It's not until client receives the trailer/RST_STREAM sent by server or rpc context has expired/cancelled, that the rpc is indeed ended.
    
    testproto: add a service for use in tests

--- internal/testproto/gen.go
+++ internal/testproto/gen.go
@@ -1,3 +1,3 @@
 package testproto
 
-//go:generate protoc -I../.. --go_out=paths=source_relative:../.. internal/testproto/test.proto
+//go:generate protoc -I../.. --go_out=paths=source_relative:../.. --go-grpc_out=paths=source_relative:../.. internal/testproto/test.proto

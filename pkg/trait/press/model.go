97303adb45313f5f9896d8d09e287b88e6d925e6
deanredfern26 <52207282+deanredfern26@users.noreply.github.com>
2025-01-03 16:42:15 +0000

    traits: add access, meter, press & temperature (#17)
    * traits: add access, meter, press & temperature
    
    * add meter info server
    
    * remove status trait from names

---
+++ pkg/trait/press/model.go
@@ -0,0 +1,57 @@
+package press
+
+import (
+	"context"
+	"time"
+
+	"github.com/smart-core-os/sc-api/go/traits"
+	"github.com/smart-core-os/sc-golang/pkg/resource"
+)
+
+type Model struct {
+	pressedState *resource.Value // of *traits.PressedState
+}
+
+func NewModel(initialPressState traits.PressedState_Press) *Model {
+	return &Model{
+		pressedState: resource.NewValue(resource.WithInitialValue(&traits.PressedState{
+			State: initialPressState,
+		})),
+	}
+}
+
+func (m *Model) GetPressedState(options ...resource.ReadOption) *traits.PressedState {
+	return m.pressedState.Get(options...).(*traits.PressedState)
+}
+
+func (m *Model) UpdatePressedState(value *traits.PressedState, options ...resource.WriteOption) (*traits.PressedState, error) {
+	updated, err := m.pressedState.Set(value, options...)
+	if err != nil {
+		return nil, err
+	}
+	return updated.(*traits.PressedState), nil
+}
+
+func (m *Model) PullPressedState(ctx context.Context, options ...resource.ReadOption) <-chan PullPressedStateChange {
+	tx := make(chan PullPressedStateChange)
+
+	rx := m.pressedState.Pull(ctx, options...)
+	go func() {
+		defer close(tx)
+		for change := range rx {
+			value := change.Value.(*traits.PressedState)
+			tx <- PullPressedStateChange{
+				Value:         value,
+				ChangeTime:    change.ChangeTime,
+				LastSeedValue: change.LastSeedValue,
+			}
+		}
+	}()
+	return tx
+}
+
+type PullPressedStateChange struct {
+	Value         *traits.PressedState
+	ChangeTime    time.Time
+	LastSeedValue bool
+}

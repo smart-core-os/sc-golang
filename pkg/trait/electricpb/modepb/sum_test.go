e8e79f95865fdb90f115c62f067433b79f321a4c
Loveen <loveen.dyall@vanti.co.uk>
2026-01-29 13:54:33 +0000

    trait/electricpb: fix tests

--- pkg/trait/electricpb/modepb/sum_test.go
+++ pkg/trait/electricpb/modepb/sum_test.go
@@ -29,7 +29,7 @@ func TestSum(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if diff := cmp.Diff(tt.want, Sum(tt.args...), protocmp.Transform()); diff != "" {
-				t.Errorf("Sum() (-want, +got)\n" + diff)
+				t.Error("Sum() (-want, +got)\n" + diff)
 			}
 		})
 	}
